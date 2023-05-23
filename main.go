1a6ef03e38fc01efe86a52553e38ff73471a538f
samrat r m <samratrm2@gmail.com>
2023-05-23 21:27:04 +0530

    module structure updated

--- main.go
+++ main.go
@@ -5,29 +5,31 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/samrat-rm/OrderService-GO.git/product"
+	"github.com/samrat-rm/OrderService-GO.git/user"
 )
 
 func initializeRouter() {
 	router := mux.NewRouter()
-	router.HandleFunc("/users", GetUsers).Methods("GET")
-	router.HandleFunc("/users", CreateUser).Methods("POST")
-	router.HandleFunc("/users/{id}", UpdateUser).Methods("PUT")
-	router.HandleFunc("/users/{id}", DeleteUser).Methods("DELETE")
-	router.HandleFunc("/users/{id}", GetUser).Methods("GET")
+	router.HandleFunc("/users", user.GetUsers).Methods("GET")
+	router.HandleFunc("/users", user.CreateUser).Methods("POST")
+	router.HandleFunc("/users/{id}", user.UpdateUser).Methods("PUT")
+	router.HandleFunc("/users/{id}", user.DeleteUser).Methods("DELETE")
+	router.HandleFunc("/users/{id}", user.GetUser).Methods("GET")
 
-	router.HandleFunc("/products", CreateProduct).Methods("POST")
-	router.HandleFunc("/products", GetAllProducts).Methods("GET")
-	router.HandleFunc("/products/{id}", GetProductByID).Methods("GET")
-	router.HandleFunc("/products/{id}", UpdateProduct).Methods("PUT")
-	router.HandleFunc("/products/{id}", DeleteProduct).Methods("DELETE")
+	router.HandleFunc("/products", product.CreateProduct).Methods("POST")
+	router.HandleFunc("/products", product.GetAllProducts).Methods("GET")
+	router.HandleFunc("/products/{id}", product.GetProductByID).Methods("GET")
+	router.HandleFunc("/products/{id}", product.UpdateProduct).Methods("PUT")
+	router.HandleFunc("/products/{id}", product.DeleteProduct).Methods("DELETE")
 
 	log.Fatal(http.ListenAndServe(":8008", router))
 }
 
 func main() {
 
-	InitialMigration()
-	InitialMigrationProduct()
+	user.InitialMigration()
+	product.InitialMigrationProduct()
 	initializeRouter()
 
 }
