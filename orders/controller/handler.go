4d13ad3df5321e35af3e5877e1eec5963f639c14
samrat r m <samratrm2@gmail.com>
2023-05-26 08:26:35 +0530

    create order handler

---
+++ orders/controller/handler.go
@@ -0,0 +1,76 @@
+package handler
+
+import (
+	"crypto/rand"
+	"encoding/json"
+	"fmt"
+	"io"
+	"math/big"
+	"net/http"
+
+	"github.com/samrat-rm/OrderService-GO.git/orders/model"
+	productModel "github.com/samrat-rm/OrderService-GO.git/product/model"
+)
+
+const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+const idLength = 6
+
+func GenerateOrderID() string {
+	id := make([]byte, idLength)
+	maxIndex := big.NewInt(int64(len(charset)))
+
+	for i := range id {
+		randomIndex, _ := rand.Int(rand.Reader, maxIndex)
+		id[i] = charset[randomIndex.Int64()]
+	}
+
+	return string(id)
+}
+
+func GetProductAmount(productID string) (*float32, error) {
+	productURL := fmt.Sprintf("http://localhost:8090/product?product_id=%s", productID)
+	resp, err := http.Get(productURL)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("failed to fetch product information. Status code: %d", resp.StatusCode)
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var product productModel.Product
+	if err := json.Unmarshal(body, &product); err != nil {
+		return nil, err
+	}
+
+	return &product.Price, nil
+}
+
+func CreateOrder(productID string, quantity int32, address string, phoneNumber string) (*float32, error) {
+	amount, err := GetProductAmount(productID)
+	if err != nil {
+		return nil, err
+	}
+
+	order := &model.Order{
+		ProductID:   productID,
+		Quantity:    quantity,
+		Address:     address,
+		PhoneNumber: phoneNumber,
+		OrderID:     GenerateOrderID(),
+	}
+
+	result := model.OrderDB.Create(order)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+
+	totalAmount := *amount * float32(quantity)
+	return &totalAmount, nil
+}
