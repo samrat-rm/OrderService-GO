01b7ecc3c055aa94e742925664e129b8a34716df
samrat r m <samratrm2@gmail.com>
2023-05-29 08:30:13 +0530

    controller and service logic seperated for create order

--- orders/controller/controller.go
+++ orders/controller/controller.go
@@ -2,11 +2,11 @@ package handler
 
 import (
 	"crypto/rand"
+	"log"
 	"math/big"
-)
 
-const (
-	idLength = 10
+	"github.com/samrat-rm/OrderService-GO.git/orders/model"
+	productModel "github.com/samrat-rm/OrderService-GO.git/orders/utils"
 )
 
 func GenerateOrderID() uint32 {
@@ -14,4 +14,51 @@ func GenerateOrderID() uint32 {
 	randomInt, _ := rand.Int(rand.Reader, maxValue)
 
 	return uint32(randomInt.Uint64())
+}
+
+func FindTotalAmount(products []*model.Product) (float64, error) {
+	totalAmount := 0.0
+	for _, product := range products {
+		var productModel productModel.Product
+		err := model.ProductDB.Where("product_id = ?", product.ProductID).First(&productModel).Error
+		if err != nil {
+			log.Print("Error fetching product:", err)
+			return 0.0, err
+		}
+
+		productAmount := float32(product.Quantity) * productModel.Price
+		totalAmount += float64(productAmount)
+
+	}
+	return totalAmount, nil
+}
+
+func CreateOrders(address string, phoneNumber string, products []*model.Product) (*model.Order, error) {
+	totalAmount, totalCalcErr := FindTotalAmount(products)
+
+	order := &model.Order{
+		Address:     address,
+		PhoneNumber: phoneNumber,
+		Products:    make([]model.Products, len(products)),
+		TotalAmount: totalAmount,
+	}
+
+	if err := model.OrderDB.Create(order).Error; err != nil || totalCalcErr != nil {
+		log.Print("Error creating order:", err)
+		return nil, err
+	}
+
+	for i, product := range products {
+		order.Products[i] = model.Products{
+			ProductID: product.ProductID,
+			Quantity:  product.Quantity,
+			OrderID:   order.ID, // Set the foreign key to the order's ID
+		}
+		if err := model.OrderDB.Create(&product).Error; err != nil {
+			log.Print("Error creating product:", err)
+			return nil, err
+		}
+	}
+
+	return order, nil
 }
--- orders/service/service.go
+++ orders/service/service.go
@@ -1,55 +1,10 @@
 package service
 
 import (
-	"log"
-
+	handler "github.com/samrat-rm/OrderService-GO.git/orders/controller"
 	"github.com/samrat-rm/OrderService-GO.git/orders/model"
-	productModel "github.com/samrat-rm/OrderService-GO.git/orders/utils"
 )
 
 func CreateOrders(address string, phoneNumber string, products []*model.Product) (*model.Order, error) {
-	order := &model.Order{
-		Address:     address,
-		PhoneNumber: phoneNumber,
-		Products:    make([]model.Products, len(products)),
-		TotalAmount: 0.0,
-	}
-
-	totalAmount := 0.0
-
-	for _, product := range products {
-		var productModel productModel.Product
-		err := model.ProductDB.Where("product_id = ?", product.ProductID).First(&productModel).Error
-		if err != nil {
-			log.Print("Error fetching product:", err)
-			return nil, err
-		}
-		log.Println(productModel.Name, "------", productModel.Price)
-		productAmount := float32(product.Quantity) * productModel.Price
-		totalAmount += float64(productAmount)
-
-	}
-	order.TotalAmount = (totalAmount)
-	if err := model.OrderDB.Create(order).Error; err != nil {
-		log.Print("Error creating order:", err)
-		return nil, err
-	}
-
-	for i, product := range products {
-		order.Products[i] = model.Products{
-			ProductID: product.ProductID,
-			Quantity:  product.Quantity,
-			OrderID:   order.ID, // Set the foreign key to the order's ID
-		}
-	}
-
-	for _, product := range order.Products {
-
-		if err := model.OrderDB.Create(&product).Error; err != nil {
-			log.Print("Error creating product:", err)
-			return nil, err
-		}
-	}
-
-	return order, nil
+	return handler.CreateOrders(address, phoneNumber, products)
 }
