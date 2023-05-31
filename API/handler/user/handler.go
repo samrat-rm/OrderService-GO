4d0abbe222aafe24f8d221f31868984c941197a7
samrat r m <samratrm2@gmail.com>
2023-05-31 09:19:50 +0530

    grpc methods for user and grpc server setup

--- API/handler/user/handler.go
+++ API/handler/user/handler.go
@@ -1,1 +1,69 @@
 package grpcHandlers
+
+import (
+	"context"
+
+	pb "github.com/samrat-rm/OrderService-GO.git/user/proto"
+	"github.com/samrat-rm/OrderService-GO.git/user/service"
+)
+
+type AuthServiceServer struct {
+	pb.AuthServiceServer
+}
+
+func (s *AuthServiceServer) SignUpUser(ctx context.Context, req *pb.SignUpRequest) (*pb.SignUpResponse, error) {
+
+	if req.Name == "" || req.Email == "" || req.Password == "" || req.PhoneNumber == "" {
+		return &pb.SignUpResponse{
+			StatusCode: 400, // Bad Request
+			Message:    "Invalid input data",
+		}, nil
+	}
+
+	access := req.Access.String()
+
+	_, err := service.CreateUser(req.Name, req.Email, req.Password, req.PhoneNumber, access)
+	if err != nil {
+
+		return &pb.SignUpResponse{
+			StatusCode: 500, // Internal Server Error
+			Message:    "Failed to sign up user",
+		}, nil
+	}
+
+	return &pb.SignUpResponse{
+		StatusCode: 200, // OK
+		Message:    "User signed up successfully",
+	}, nil
+}
+
+func (s *AuthServiceServer) LoginUser(ctx context.Context, req *pb.LoginRequest) (*pb.LoginResponse, error) {
+	// Implement the logic for user login here
+	// You can access the request parameters using req.Email and req.Password
+
+	// Perform the necessary operations such as validating credentials, generating a token, generating a response, etc.
+
+	// Create the response message
+	response := &pb.LoginResponse{
+		StatusCode: 200,                // Set the appropriate status code
+		Message:    "Login successful", // Set the appropriate message
+		Token:      "generated-token",  // Set the generated token value
+	}
+
+	return response, nil
+}
+
+func (s *AuthServiceServer) ValidateToken(ctx context.Context, req *pb.ValidateTokenRequest) (*pb.ValidateTokenResponse, error) {
+	// Implement the logic for token validation here
+	// You can access the request parameters using req.Token and req.Access
+
+	// Perform the necessary operations such as validating the token, checking access level, generating a response, etc.
+
+	// Create the response message
+	response := &pb.ValidateTokenResponse{
+		StatusCode: 200,                           // Set the appropriate status code
+		Message:    "Token validation successful", // Set the appropriate message
+	}
+
+	return response, nil
+}
--- user/main.go
+++ user/main.go
@@ -1,1 +1,55 @@
-package user
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"os"
+
+	"github.com/joho/godotenv"
+	"google.golang.org/grpc"
+
+	grpcHandlers "github.com/samrat-rm/OrderService-GO.git/API/handler/user"
+	"github.com/samrat-rm/OrderService-GO.git/user/model"
+
+	pb "github.com/samrat-rm/OrderService-GO.git/user/proto"
+)
+
+// var (
+// 	port = ":8091"
+// )
+
+func initEnv() {
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatalf("Failed to laod env")
+	}
+}
+
+func main() {
+
+	initEnv()
+	port := os.Getenv("PORT")
+	database := os.Getenv("Database")
+	userName := os.Getenv("UserName")
+	password := os.Getenv("Password")
+
+	DNS := fmt.Sprintf("host=localhost port=5434 user=%s password=%s dbname=%s sslmode=disable", userName, password, database)
+	err := model.InitDB(DNS)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer model.CloseDB()
+
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("Faile to start the server %v", err.Error())
+	}
+	grpcServer := grpc.NewServer()
+	pb.RegisterAuthServiceServer(grpcServer, &grpcHandlers.AuthServiceServer{})
+	log.Printf("Server satrted at %v", lis.Addr())
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("Failed to start %v", err.Error())
+	}
+}
